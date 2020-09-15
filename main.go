c9ae0b01c2743c7bba5636aec529819a8f20533d
Nicolas A Perez <anicolaspp@gmail.com>
2020-09-14 21:11:45 -0400

    Update main.go

--- main.go
+++ main.go
@@ -27,45 +27,6 @@ func main() {
 	}
 }
 
-//func handleConnection(conn net.Conn) {
-//	// close connection on exit
-//	defer conn.Close()
-//
-//	conn.Write([]byte("220 Welcome to Nico FTP Server\n"))
-//
-//	cmds := make(chan []byte)
-//
-//	var buf [512]byte
-//	for {
-//		// read upto 512 bytes
-//		n, err := conn.Read(buf[0:])
-//
-//		fmt.Println(n)
-//
-//		if err != nil {
-//			fmt.Println(err)
-//		}
-//
-//		cmd := string(buf[0:n])
-//
-//		fmt.Println(cmd)
-//
-//		go commands.ProcessCommand(buf[0:n], cmds)
-//
-//		reponse := <-cmds
-//
-//		fmt.Println("Sending: " + string(reponse))
-//
-//		conn.Write(reponse)
-//
-//		// write the n bytes read
-//		_, err2 := conn.Write(buf[0:n])
-//		if err2 != nil {
-//			return
-//		}
-//	}
-//}
-
 func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
